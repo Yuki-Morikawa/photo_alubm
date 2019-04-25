843117bcda9faa4c465e24e95fa15f0addea153c
Yuki-Morikawa <myrukuwi@gmail.com>
2019-04-25 16:54:33 +0000

    cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@
 ActiveRecord::Schema.define(version: 20181206115853) do
 
   create_table "contributions", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.string   "body"
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "img"
